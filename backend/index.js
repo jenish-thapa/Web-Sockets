435c4acb505f0107ecaf085b643e0a882e3aa6bd
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-07-13 17:25:29 +0530

    Added socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
 });
 
 server.listen(3000, () => console.log("Server running at PORT: 3000"));
